e977b5abc53052276dcea45e2f7adbce1ad1e9dd
Duc Thai <ducthai060501@gmail.com>
2021-12-23 22:46:56 +0200

    using redux data

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,20 @@
+import { useSelector} from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+
+  //pass a a func into useSelector, a fuc that decide the data we want to take
+  const counter = useSelector(state => state.counter) ;
+  // auto set up a subcription, compo will receive the changes automatically
+
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
