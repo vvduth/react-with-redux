5bbe3c8ad601b80e8f15f9a729de1e268e52a8c9
Duc Thai <ducthai060501@gmail.com>
2021-12-23 23:52:28 +0200

    Add payload

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,80 +4,84 @@ import { useSelector , useDispatch, connect} from 'react-redux';
 
 import classes from './Counter.module.css';
 
-// const Counter = () => {
+const Counter = () => {
 
-//   const dispatch = useDispatch();
-//   // use dispatch return a function that can be executed
+  const dispatch = useDispatch();
+  // use dispatch return a function that can be executed
 
-//   //pass a a func into useSelector, a fuc that decide the data we want to take
-//   const counter = useSelector(state => state.counter) ;
-//   // auto set up a subcription, compo will receive the changes automatically
+  //pass a a func into useSelector, a fuc that decide the data we want to take
+  const counter = useSelector(state => state.counter) ;
+  // auto set up a subcription, compo will receive the changes automatically
 
-//   const minusHanlder = () =>{
-//     dispatch({type:'decrement' });
-//   }
-
-//   const addHandler = () => {
-//       dispatch({type:'increment' });
-//   }
-
-//   const toggleCounterHandler = () => {};
-
-//   return (
-//     <main className={classes.counter}>
-//       <h1>Redux Counter</h1>
-//       <div className={classes.value}>{counter}</div>
-//       <div>
-//         <button onClick={addHandler}>Add</button>
-//         <button onClick={minusHanlder}>Minus</button>
-//       </div>
-//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
-//     </main>
-//   );
-// };
-
-class Counter extends Component {
-
-  // connect hook helps connecting class based-compo to central data
-  addHandler() {
-    this.props.increment() ;
-  };
-
-  minusHanlder() {
-    this.props.decrement() ;
-  };
-
-  toggleCounterHandler() {};
-  render() {
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div className={classes.value}>{this.props.counter}</div>
-        <div>
-          <button onClick={this.addHandler.bind(this)}>Add</button>
-          <button onClick={this.minusHanlder.bind(this)}>Minus</button>
-        </div>
-        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-      </main>
-    );
+  const minusHanlder = () =>{
+    dispatch({type:'decrement' });
   }
-}
 
-const mapStateToProps = state => {
-  return {
-    counter: state.counter
-  };
-}
+  const increaseHandler = () => {
+    dispatch({type: 'increase', amount: 5})
+  }
+  const addHandler = () => {
+      dispatch({type:'increment' });
+  }
 
-const mapDisptachToProps = dispatch => {
-  return  {
-    increment: () =>  dispatch({type: 'increment'}) ,
-    decrement : () => dispatch({type: 'decrement'}) ,
-  };
-}
-//export default Counter; // dis for functional compo
+  const toggleCounterHandler = () => {};
 
-export default connect(mapStateToProps,mapDisptachToProps)(Counter) ;
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={addHandler}>Add</button>
+        <button onClick={increaseHandler}>Add 5 </button>
+        <button onClick={minusHanlder}>Minus</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+// class Counter extends Component {
+
+//   // connect hook helps connecting class based-compo to central data
+//   addHandler() {
+//     this.props.increment() ;
+//   };
+
+//   minusHanlder() {
+//     this.props.decrement() ;
+//   };
+
+//   toggleCounterHandler() {};
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.addHandler.bind(this)}>Add</button>
+//           <button onClick={this.minusHanlder.bind(this)}>Minus</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     counter: state.counter
+//   };
+// }
+
+// const mapDisptachToProps = dispatch => {
+//   return  {
+//     increment: () =>  dispatch({type: 'increment'}) ,
+//     decrement : () => dispatch({type: 'decrement'}) ,
+//   };
+// }
+export default Counter; // dis for functional compo
+
+//export default connect(mapStateToProps,mapDisptachToProps)(Counter) ;
 
 // connect when executed returns a new fuc and excetuced it as a value 
 // pass counter to the new function
